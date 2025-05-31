44b395425c057c932adf708b95691852abedee44
renato086 <121212622+renato086@users.noreply.github.com>
2025-05-31 17:03:12 -0300

    Update FeiraVendasApp.jsx

--- src/FeiraVendasApp.jsx
+++ src/FeiraVendasApp.jsx
@@ -48,7 +48,7 @@ export default function FeiraVendasApp() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
     <div className="flex justify-center mb-4">
-      <img src={logo} alt="Logo Mundo Nerd" className="h-[3cm] max-w-[5cm] object-contain" />
+     <img src={logo} alt="Logo Mundo Nerd" className="h-[60px] w-auto max-w-[160px] object-contain" />
     </div>
       <div className="max-w-3xl mx-auto bg-white/80 rounded-xl p-4 shadow-md">
         <h1 className="text-2xl font-bold mb-4">🚀Mundo Nerd br🚀</h1>
