5c4ecdb3db5600595170681ba0f9060bb5cbedf7
renato086 <121212622+renato086@users.noreply.github.com>
2025-05-31 07:51:24 -0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Add additional Firebase SDKs as needed (e.g., Firestore, Auth)
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx7degWRAyjKoey5vrsKXDeMD4EXegWcc",
+  authDomain: "mundonerd-app.firebaseapp.com",
+  projectId: "mundonerd-app",
+  storageBucket: "mundonerd-app.appspot.com",
+  messagingSenderId: "507296290570",
+  appId: "1:507296290570:web:6ad22a11d99055cb124cf3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
